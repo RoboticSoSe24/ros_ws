6b6b39d759c1db069d2c60ec6c8bf57d5b7e2afd
Martin Werner <m.werner.0@protonmail.com>
2024-04-09 19:19:12 +0200

    Handle perspective transformation in laneObserver (#3)

--- src/perception/perception/laneObserver.py
+++ src/perception/perception/laneObserver.py
@@ -49,9 +49,50 @@ class LaneObserver(Node):
 
 
     def image_callback(self, data):
-        # retrieve and display camera image
+        # retrieve camera image
         img_cv = self.bridge.compressed_imgmsg_to_cv2(data, desired_encoding='passthrough')
-        cv2.imshow("img", img_cv)
+        
+        # get light channel
+        img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2HLS)
+        img_cv = cv2.split(img_cv)[1]
+
+        # pad into bigger matrix
+        sub = np.zeros((960, 1280, 1), dtype = "uint8")
+        sub[720:960, 480:800, 0] = img_cv
+
+        pts1 = np.float32([
+            [105 + 480, 178 + 720], 
+            [190 + 480, 178 + 720], 
+            [67  + 480, 231 + 720], 
+            [225 + 480, 233 + 720]])
+        
+        pts2 = np.float32([
+            [480,720], 
+            [800,720], 
+            [480,img_cv.shape[0]+720], 
+            [img_cv.shape[1]+480,img_cv.shape[0]+720]])
+        
+        # perspective transform
+        M = cv2.getPerspectiveTransform(pts1,pts2)        
+        dst = cv2.warpPerspective(sub, M, (1280, 960))
+
+
+        dst = cv2.resize(dst, None, fx = 0.5, fy = 0.5)
+        ret, thresh = cv2.threshold(dst, 120, 255, cv2.THRESH_BINARY)
+
+        # find line segments in threshold image
+        linesP = cv2.HoughLinesP(thresh, 1, np.pi / 180.0, 50, None, 30, 10)
+        cdstP = np.zeros((480, 640, 3), dtype = "uint8")
+        if linesP is not None:
+            for i in range(0, len(linesP)):
+                l = linesP[i][0]
+                cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+        
+
+        cv2.imshow("img", sub)
+        cv2.imshow("warped", dst)
+        cv2.imshow("thresh", thresh)
+        cv2.imshow("line segments", cdstP)
         cv2.waitKey(1)
 
 
