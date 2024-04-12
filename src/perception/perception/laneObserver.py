7fe30b11a9fa656ad99bd1a7f1f7aad57d97a63d
Martin Werner <m.werner.0@protonmail.com>
2024-04-12 21:39:20 +0200

    Add relevant parameters to filter lanes

--- src/perception/perception/laneObserver.py
+++ src/perception/perception/laneObserver.py
@@ -18,8 +18,14 @@ class LaneObserver(Node):
     def __init__(self):
         super().__init__('lane_observer')
 
-        # create publisher for topic 'lanes'
-        self.lanePublisher_ = self.create_publisher(PoseArray, 'lanes', 10)
+        # declare parameters
+        self.declare_parameter('canny_threshold', 50)
+        self.declare_parameter('line_width', 16)
+        self.declare_parameter('lane_width', 89)
+
+        # create timer for updating parameter values
+        self.parameterTimer = self.create_timer(3, self.parameter_callback)
+        self.parameter_callback()
 
         # init openCV-bridge
         self.bridge = CvBridge()
@@ -37,19 +43,27 @@ class LaneObserver(Node):
             qos_profile=qos_policy)
         self.subscription  # prevent unused variable warning
 
+        # create publisher for topic 'lanes'
+        self.lanePublisher_ = self.create_publisher(PoseArray, 'lanes', 10)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        #self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info('initialized laneObserver')
+
+
+    def parameter_callback(self):
+        # fetch parameters
+        self.cannyThreshold = self.get_parameter('canny_threshold').get_parameter_value().double_value
+        self.lineWidth      = self.get_parameter('line_width').get_parameter_value().integer_value
+        self.laneWidth      = self.get_parameter('lane_width').get_parameter_value().integer_value
+        self.get_logger().info('parameters updated')
 
 
     def image_callback(self, data):
         imSize = (320, 240)
         shape1C = (imSize[1], imSize[0], 1)
         shape3C = (imSize[1], imSize[0], 3)
+
+        canvas1C = np.zeros(shape1C, dtype='uint8')
+        canvas3C = np.zeros(shape3C, dtype='uint8')
 
         # retrieve camera image
         img_cv = self.bridge.compressed_imgmsg_to_cv2(data, desired_encoding='passthrough')
@@ -58,11 +72,12 @@ class LaneObserver(Node):
         # get lightness channel
         img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2HLS)
         img_cv = cv2.split(img_cv)[1]
+        cv2.imshow("source", img_cv)
+
 
         # pad into bigger matrix
-        sub = np.zeros((shape1C[0]*4, shape1C[1]*4, 1), dtype = "uint8")
+        sub = np.zeros((shape1C[0]*4, shape1C[1]*4, 1), dtype = 'uint8')
         sub[720:960, 480:800, 0] = img_cv
-        cv2.imshow("padded src", sub)
 
         # perspective transform
         pts1 = np.float32([             # source points (measured prior)
@@ -82,36 +97,39 @@ class LaneObserver(Node):
         warp = cv2.GaussianBlur(warp, (5, 5), 0)
         cv2.imshow("warped", warp)
 
-        # find edges
-        edges = cv2.Canny(warp, 40, 150)
-        cv2.imshow("canny", edges)
 
-        # connect edges to lines through dilation
-        dilate = cv2.dilate(edges, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10)))
+        # find all lines with correct width 
+        edges = cv2.Canny(warp, 60, 150)                                # get all edges
+        dilate = cv2.dilate(edges, cv2.getStructuringElement(           # connect close enough edges to lines through dilation
+            cv2.MORPH_ELLIPSE, (int(self.lineWidth/2), int(self.lineWidth/2))))  
+        erode = cv2.erode(dilate, cv2.getStructuringElement(            # remove lines thinner than minimum width through erosion
+            cv2.MORPH_ELLIPSE, (self.lineWidth-2,      self.lineWidth-2)))   
+        canvas3C[:,:,0] = dilate
+        canvas3C[:,:,1] = erode
+        canvas3C[:,:,2] = edges
+        cv2.imshow("dilation, erosion, edges", canvas3C)
 
-        # remove lines thinner than minimum width through erosion
-        erode = cv2.erode(dilate, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (16, 16)))
-        cv2.imshow("dilate_erode", erode)
 
-        # dilate to connect dotted line into one contour
-        erode = cv2.dilate(erode, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (40, 40)))   
-
-        # get remaining contours
-        contours,_ = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        erode = cv2.dilate(erode, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (40, 40)))   # dilate to connect dotted line into one contour
+        contours,_ = cv2.findContours(erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)        # get remaining contours
         if contours is None or len(contours) < 2:
             return
         
         # determine centerline as overlap between the two largest lines
         contours = sorted(contours, key=lambda cnt: cv2.contourArea(cnt), reverse=True)[:2]
-        img1 = np.zeros(shape1C, dtype="uint8")
-        cv2.drawContours(img1, [contours[0]], 0, (255,255,255), 87)     # dilate to achieve an overlap with the other line
-        img2 = np.zeros(shape1C, dtype="uint8")
-        cv2.drawContours(img2, [contours[1]], 0, (255,255,255), 87)     # dilate to achieve an overlap with the other line
-        centerImg = cv2.ximgproc.thinning(img1 & img2, 0)               # binary AND to get only overlap region
-        cv2.imshow("centerline", centerImg)
+        img1 = np.zeros(shape1C[:2], dtype='uint8')
+        cv2.drawContours(img1, [contours[0]], 0, (255,255,255), self.laneWidth)     # dilate to achieve an overlap with the other line
+        img2 = np.zeros(shape1C[:2], dtype='uint8')
+        cv2.drawContours(img2, [contours[1]], 0, (255,255,255), self.laneWidth)     # dilate to achieve an overlap with the other line
+        centerImg = cv2.ximgproc.thinning(img1 & img2, 0)                           # binary AND to get only overlap region
+        cv2.rectangle(centerImg, (0,0), imSize, (0,0,0), 2)                         # mask out points on the edges of the image 
+        canvas3C[:,:,0] = img1 * 0.5 + (img1 & img2) * 0.25 - centerImg
+        canvas3C[:,:,1] = img2 * 0.5 + (img1 & img2) * 0.25 - centerImg
+        canvas3C[:,:,2] = centerImg
+        cv2.imshow("contour 1, contour 2, centerline", canvas3C)
         
-        # get line as segments
-        linesP = cv2.HoughLinesP(centerImg, 1, np.pi / 360, 30, None, 30, 5)
+
+        linesP = cv2.HoughLinesP(centerImg, 1, np.pi / 360, 30, None, 30, 5)    # get line as segments
         if linesP is None:
             return
         
@@ -125,15 +143,15 @@ class LaneObserver(Node):
         array.sort(key=lambda pt: np.linalg.norm(pt))
 
         # draw final points to image
-        final = np.zeros(shape3C, dtype="uint8")
+        canvas3C = np.zeros(shape3C, dtype='uint8')
         for i in range(len(array)):
             pt1 = (array[i][0]+160, 240-array[i][1])
-            cv2.circle(final, pt1, 2, (0,0,255))
-            cv2.putText(final, str(i), pt1, cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,255,0))
+            cv2.circle(canvas3C, pt1, 2, (0,0,255))
+            cv2.putText(canvas3C, str(i), pt1, cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,255,0))
             if i < len(array)-1:
                 pt2 = (array[i+1][0]+160, 240-array[i+1][1])
-                cv2.line(final, pt1, pt2, (0,255,0))
-        cv2.imshow("final points", final)
+                cv2.line(canvas3C, pt1, pt2, (0,255,0))
+        cv2.imshow("final points", canvas3C)
 
         # publish points to 'lanes'
         poseArr = PoseArray()
