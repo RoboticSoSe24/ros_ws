6cd6a76a63c121b5ffef43dc232ea92b5039aa3c
BenWeckend <ben.weckend@web.de>
2024-04-12 19:26:40 +0200

    small changes

--- src/planning/planning/defaultDriving.py
+++ src/planning/planning/defaultDriving.py
@@ -15,10 +15,11 @@ class DefaultDriving(Node):
 
         # definition of the parameters that can be changed at runtime
         self.declare_parameter('distance_to_stop', 0.3)
-        self.declare_parameter('boundary_left', -11)
-        self.declare_parameter('boundary_right', 11)
+        self.declare_parameter('boundary_left', -12)
+        self.declare_parameter('boundary_right', 12)
         self.declare_parameter('speed_drive', 0.1)
-        self.declare_parameter('speed_turn', 0.4)
+        self.declare_parameter('speed_drive_fast', 0.2)
+        self.declare_parameter('speed_turn', 0.35)
 
         # variable for the last sensor reading
         self.min_index = 0
@@ -31,29 +32,29 @@ class DefaultDriving(Node):
                                          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
                                          depth=1)
         
-        # create subscribers for laser scan data
-        self.subscription = self.create_subscription(
+        # create subscribers
+        self.laser_subscription = self.create_subscription(
             LaserScan,
             'scan',
             self.scanner_callback,
             qos_profile=qos_policy)
-        self.subscription  # prevent unused variable warning
+        self.laser_subscription  
 
-        # Initialize m as None
-        self.m = None
-
-        self.subscription = self.create_subscription(
+        self.lane_subscription = self.create_subscription(
             PoseArray,
             'lanes',  # Name des Topics
             self.listener_callback,
             10)
-        self.subscription
+        self.lane_subscription
 
+        self.m = None
+        
         # create publisher for driving commands
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 1)
         
-        timer_period = 0.5  # seconds
+        timer_period = 0.1  # seconds
         self.my_timer = self.create_timer(timer_period, self.timer_callback)
+
 
     def scanner_callback(self, msg):
         laser_data = msg.ranges
@@ -64,6 +65,7 @@ class DefaultDriving(Node):
             if x != 0.0 and x < self.min_value:
                 self.min_value = x
                 self.min_index = i
+        self.get_logger().info('laserscan callback')
 
 
     def listener_callback(self, msg):
@@ -80,11 +82,17 @@ class DefaultDriving(Node):
             boundary_left = self.get_parameter('boundary_left').get_parameter_value().integer_value
             boundary_right = self.get_parameter('boundary_right').get_parameter_value().integer_value
             speed_drive = self.get_parameter('speed_drive').get_parameter_value().double_value
+            speed_drive_fast = self.get_parameter('speed_drive').get_parameter_value().double_value
             speed_turn = self.get_parameter('speed_turn').get_parameter_value().double_value
 
             if self.min_value < distance_stop:
                 speed_drive = 0.0
                 speed_turn = 0.0
+                self.get_logger().info(str(self.min_value))
+
+
+            if self.m > boundary_left and self.m < boundary_right:
+                speed_drive = speed_drive_fast
 
             if self.m < boundary_left:
                 self.get_logger().info('Objekt={} // Index={} // links fahren!    m={}'.format(round(float(self.min_value), 2), self.min_index, self.m))
@@ -95,7 +103,6 @@ class DefaultDriving(Node):
             else: 
                 self.get_logger().info('Objekt={} // Index={} // geradaus fahren! m={}'.format(round(float(self.min_value), 2), self.min_index, self.m))
                 turn = 0.0
-
 
             msg = Twist()
             msg.linear.x = speed_drive
